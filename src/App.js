ab761a3ca656aa6fac0a0d6df8d11c5adb2e9ad6
TDgithub1 <105603327+TDgithub1@users.noreply.github.com>
2023-06-28 17:58:03 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,9 +2,33 @@ import React from 'react';
 import './style.css';
 
 export default function App() {
+  // let clickText = 'amila';
+  // const [clickText, setClickText] = useState('tdp');
+
+  const [myVar, setMyvar] = useState('Amila');
+
+  const clickHandle = () => {
+    // clickText = 'Thisara';
+    setMyvar('Thisara');
+  };
+
   return (
     <div>
       <h1>Hello TDP</h1>
+
+      <h2>{myVar}</h2>
+
+      <button
+        style={{
+          fontSize: '14px',
+          border: '1px solid red',
+          padding: '7px 12px',
+        }}
+        onClick={clickHandle}
+      >
+        Click me
+      </button>
+
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
